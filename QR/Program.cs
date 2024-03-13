f3cd9b4a47600af78b588928a570abb0a26fc8cf
Julián Podbersich <32556062+JPDKR@users.noreply.github.com>
2024-03-13 11:58:02 -0300

    Add files via upload

---
+++ QR/Program.cs
@@ -0,0 +1,14 @@
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+
+string outputFile = @"C:\Users\Julian Podbersich\Downloads\prueba.pdf";
+string url = "https://www.metal-archives.com/bands/kreator/157";
+
+Document doc = new(PageSize.A4);
+PdfWriter.GetInstance(doc, new FileStream(outputFile, FileMode.Create));
+doc.Open();
+BarcodeQRCode barcodeQRCode = new(url, 1000, 1000, null);
+Image codeQRImage = barcodeQRCode.GetImage();
+codeQRImage.ScaleAbsolute(200, 200);
+doc.Add(codeQRImage);
+doc.Close();
